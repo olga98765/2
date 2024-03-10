9a0ab3b38eadba9ec4b628f159af0d0b3b0605c4
ola <o.monko@o2.pl>
2024-03-10 18:22:18 +0100

    array created

--- ex22.java
+++ ex22.java
@@ -1,11 +1,14 @@
 // [10:09] Filip Kuca AEH
 // 1.2) Napisz funkcję, która przyjmuje zakres liczb i zwraca tablicę liczb pierwszych w tym zakresie.
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ex22 {
 
     public static void main(String[] args) {
    int[] numbersArray = {1,2,3,4,5,6,7,11,21,23,60};
-int[] primeNumbersArray;
+ArrayList<Integer> primeNumbersList = new ArrayList<>();
 
 
    for (int number : numbersArray) {
@@ -20,13 +23,24 @@ int[] primeNumbersArray;
           break;
         }
       }
+     
+
+      if (prime == true) {
+        primeNumbersList.add(number);
+
+        System.out.println(primeNumbersList);
+        // int[] primeNumbersArray = new int[number];
+
+// Arrays.fill(primeNumbersArray, number);
 
 
+      }
+   
+      
 
     
 }
 
-    
     
 }
     }
