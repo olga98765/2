c3be94de4d06f19dfd9a53669c93532e81c2f746
ola <o.monko@o2.pl>
2024-03-10 13:36:59 +0100

    ex23 completed

--- ex23.java
+++ ex23.java
@@ -5,16 +5,18 @@ public class ex23 {
 
     public static void main(String[] args) {
         int[] numbersArray = { 1, 2, 3, 6, 5, 1, 2, 3, 2, 1, 1 };
-        int number = 6;
+        int number = 15;
         int sum = 0;
 
         for (int numberArray : numbersArray) {
             if (numberArray == number) {
                 sum++;
                 System.out.println(sum);
+                break;
+            } else {
+                System.out.println("Liczba nie występuje w tablicy");
+                break;
             }
         }
-
     }
-
 }
