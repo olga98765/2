cd562781799a18905b9f9912fee2540acc834445
ola <o.monko@o2.pl>
2024-03-10 17:31:15 +0100

    logic

--- ex22.java
+++ ex22.java
@@ -5,22 +5,36 @@ public class ex22 {
 
     public static void main(String[] args) {
    int[] numbersArray = {1,2,3,4,5,6,7,11,21,23,60};
-// jezeli liczba dzieli sie przez 1 i sama siebie i brak innych dzielnikow to
-// 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 101 103 107 109 113 127 131 137 139 149 151 157 163 167 173 179 181 191 193 197 199 211 223 227 229 233 239 241 251 257 263 269 271 277 281 283 293 307 311 313 317 331 337 347 349 353 359 367 373 379 383 389 397 401 409 419 421 431 433 439 443 449 457 461 463 467 479 487 491 499 503 509 521 523 541 547
 
 
 
+   for (int number : numbersArray) {
+    boolean prime = true;
 
-   for (int number :numbersArray) {
+    if (number <= 1) {
+      prime = false;
+    } else {
+      for (int i = 2; i * i <= number; i++) {
+        if (number % i == 0) {
+          prime = false;
+          break;
+        }
+      }
+    }
 
-
-
+    System.out.println(prime ? "pierwsza" : "");
+  }
+}
 
 }
 
 
 
 
-   }
 
-    }
+
+
+
+   
+
+    
