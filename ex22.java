82aba314faafb1d66dce4b7f112db160bfc768d6
ola <o.monko@o2.pl>
2024-03-10 18:23:01 +0100

    format

--- ex22.java
+++ ex22.java
@@ -7,56 +7,32 @@ import java.util.Arrays;
 public class ex22 {
 
     public static void main(String[] args) {
-   int[] numbersArray = {1,2,3,4,5,6,7,11,21,23,60};
-ArrayList<Integer> primeNumbersList = new ArrayList<>();
+        int[] numbersArray = { 1, 2, 3, 4, 5, 6, 7, 11, 21, 23, 60 };
+        ArrayList<Integer> primeNumbersList = new ArrayList<>();
 
+        for (int number : numbersArray) {
+            boolean prime = true;
 
-   for (int number : numbersArray) {
-    boolean prime = true;
+            if (number <= 1) {
+                prime = false;
+            } else {
+                for (int i = 2; i * i <= number; i++) {
+                    if (number % i == 0) {
+                        prime = false;
+                        break;
+                    }
+                }
 
-    if (number <= 1) {
-      prime = false;
-    } else {
-      for (int i = 2; i * i <= number; i++) {
-        if (number % i == 0) {
-          prime = false;
-          break;
+                if (prime == true) {
+                    primeNumbersList.add(number);
+
+                    System.out.println(primeNumbersList);
+
+                }
+
+            }
+
         }
-      }
-     
-
-      if (prime == true) {
-        primeNumbersList.add(number);
-
-        System.out.println(primeNumbersList);
-        // int[] primeNumbersArray = new int[number];
-
-// Arrays.fill(primeNumbersArray, number);
-
-
-      }
-   
-      
-
-    
-}
-
-    
-}
     }
-    
 
-  
-  }
-
-
-
-
-
-
-
-
-
-   
-
-    
+}
