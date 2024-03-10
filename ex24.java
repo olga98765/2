c9fb17a2ace9943d017a1a1151afa51a10942eb1
ola <o.monko@o2.pl>
2024-03-10 14:07:30 +0100

    format

--- ex24.java
+++ ex24.java
@@ -4,8 +4,13 @@
 public class ex24 {
 
     public static void main(String[] args) {
-   
+        int number = 10;
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            sum += i;
 
-        System.out.println();
+        }
+        System.out.println(sum);
+
     }
 }
