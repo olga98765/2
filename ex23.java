0661128ccf41ae63a18496d06f242f7c44b28427
ola <o.monko@o2.pl>
2024-03-10 19:11:49 +0100

    ex23 completed

--- ex23.java
+++ ex23.java
@@ -1,22 +1,16 @@
-// [10:10] Filip Kuca AEH
 // 1.3) Napisz funkcję, która przyjmuje tablicę i liczbę, a następnie zlicza ile razy ta liczba występuje w tablicy.
+
+import java.util.Arrays;
 
 public class ex23 {
 
     public static void main(String[] args) {
         int[] numbersArray = { 1, 2, 3, 6, 5, 1, 2, 3, 2, 1, 1 };
-        int number = 15;
-        int sum = 0;
+        int number = 1;
 
-        for (int numberArray : numbersArray) {
-            if (numberArray == number) {
-                sum++;
-                System.out.println(sum);
-                break;
-            } else {
-                System.out.println("Liczba nie występuje w tablicy");
-                break;
-            }
-        }
+        long count = Arrays.stream(numbersArray).filter(n -> n == number).count();
+
+        System.out.println("Liczba " + number + " występuje w tablicy " + count + " razy.");
     }
+
 }
